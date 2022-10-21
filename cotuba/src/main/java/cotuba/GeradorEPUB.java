fab01deb3db4f18cd08613879e74846dbe149689
Gilson Silva <gilsonsilvati@gmail.com>
2022-10-21 17:35:10 -0300

    Iniciando implemetações para adequar ao Single Responsibility Principle (SRP). :hammer:

---
+++ cotuba/src/main/java/cotuba/GeradorEPUB.java
@@ -0,0 +1,31 @@
+package cotuba;
+
+import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.domain.Resource;
+import nl.siegmann.epublib.epub.EpubWriter;
+import nl.siegmann.epublib.service.MediatypeService;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class GeradorEPUB {
+
+    public void gera(Path diretorioDosMD, Path arquivoDeSaida) {
+
+        var epub = new Book();
+
+        // TODO: usar título do capítulo
+        epub.addSection("Capítulo", new Resource(html.getBytes(), MediatypeService.XHTML));
+
+        var epubWriter = new EpubWriter();
+
+        try {
+            epubWriter.write(epub, Files.newOutputStream(arquivoDeSaida));
+        } catch (IOException ex) {
+            throw new IllegalStateException("Erro ao criar arquivo EPUB: " + arquivoDeSaida.toAbsolutePath(), ex);
+        }
+
+    }
+
+}
